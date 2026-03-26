909c1d21e9191378b5a0c362b1e449f3eaaf734f
ners <ners@gmx.ch>
2026-03-26 11:00:13 +0100

    improve colour section

--- dashi/app/Section/Colours.hs
+++ dashi/app/Section/Colours.hs
@@ -4,27 +4,30 @@
 module Section.Colours where
 
 import Dashi.Components.Heading
-import Dashi.Prelude hiding (Simple, update, view)
-import Dashi.Style.Colour (Linearity (..), SRGB, pattern ColorSRGB)
+import Dashi.Prelude hiding (Simple, simple, update, view)
+import Dashi.Style.Colour
+    ( Color
+    , Linearity (..)
+    , OKLCH
+    , SRGB
+    , pattern ColorOKLCH
+    , pattern ColorSRGB
+    )
 import Dashi.Style.Colour qualified as Colour
 import Dashi.Style.Tokens
-import Dashi.Style.Uchu (Uchu, uchu)
-import Dashi.Util (breakAll, capitalise)
-import Data.Char (isDigit, isUpper)
-import Data.List qualified as List
 import Data.Word (Word8)
+import Graphics.Color.Uchu
+import Graphics.Color.Uchu.Simple.OKLCH ()
+import Miso.CSS (styleInline_)
 import Miso.Html.Element
     ( a_
+    , div_
+    , li_
     , p_
     , section_
-    , table_
-    , tbody_
-    , td_
-    , th_
-    , thead_
-    , tr_
+    , ul_
     )
-import Miso.Html.Property (href_)
+import Miso.Html.Property (class_, href_)
 import Miso.String qualified as MisoString
 import Text.Printf (printf)
 
@@ -55,54 +58,130 @@ view Model =
             , text " to create consistency in apps."
             ]
         , widget $ Heading Medium "Simple"
-        , table_
-            []
-            [ thead_
-                []
-                [ th_ [] [text "Colour"]
-                , th_ [] [text "OKLCH"]
-                , th_ [] [text "RGB"]
-                , th_ [] [text "Hex"]
-                ]
-            , tbody_ [] $ row <$> simples
+        , ul_
+            [class_ "palette"]
+            [ simple "Gray" uchu.gray
+            , simple "Red" uchu.red
+            , simple "Pink" uchu.pink
+            , simple "Purple" uchu.purple
+            , simple "Blue" uchu.blue
+            , simple "Green" uchu.green
+            , simple "Yellow" uchu.yellow
+            , simple "Orange" uchu.orange
             ]
         , widget $ Heading Medium "Extended"
-        , table_
-            []
-            [ thead_
-                []
-                [ th_ [] [text "Colour"]
-                , th_ [] [text "OKLCH"]
-                , th_ [] [text "RGB"]
-                , th_ [] [text "Hex"]
-                ]
-            , tbody_ [] $ row <$> expandeds
+        , ul_
+            [class_ "palette"]
+            [ extended "Gray" uchu.gray
+            , extended "Red" uchu.red
+            , extended "Pink" uchu.pink
+            , extended "Purple" uchu.purple
+            , extended "Blue" uchu.blue
+            , extended "Green" uchu.green
+            , extended "Yellow" uchu.yellow
+            , extended "Orange" uchu.orange
             ]
         ]
 
-simples, expandeds :: [Uchu]
-(expandeds, simples) = List.partition (isDigit . last . tokenName) [minBound .. maxBound]
-
-row :: Uchu -> View Model Action
-row u =
-    tr_
+simple :: MisoString -> Simple OKLCH Micro -> View Model Action
+simple name oklch =
+    li_
         []
-        [ td_
-            []
-            [ text
-                . capitalise
-                . MisoString.intercalate " "
-                . breakAll (\c -> isUpper c || isDigit c)
-                . ishow
-                $ u
+        [ widget $ Heading Small name
+        , ul_
+            [class_ "colour"]
+            [ shade "Light"
+            , shade "Base"
+            , shade "Dark"
             ]
-        , td_ [] [text . toMisoString $ oklch]
-        , td_ [] [text rgbDec]
-        , td_ [] [text rgbHex]
         ]
   where
-    oklch = uchu @Micro u
-    ColorSRGB r g b = Colour.convertColor @(SRGB 'NonLinear) $ uchu @Double u
+    shade prefix =
+        let
+            bg, fg :: MisoString
+            bg = "background-color: " <> var prefix
+            fg =
+                case prefix of
+                    "Dark" -> "color: var(--uchu-yang)"
+                    _ -> "color: var(--uchu-yin)"
+         in
+            li_
+                [ class_ "shade"
+                , styleInline_ $ MisoString.intercalate ";" [bg, fg]
+                ]
+                [ div_ [class_ "name"] [text prefix]
+                , codes
+                    $ case prefix of
+                        "Dark" -> oklch.dark
+                        "Light" -> oklch.light
+                        _ -> oklch.base
+                ]
+    var "Base" = "var(--uchu-" <> MisoString.toLower name <> ")"
+    var prefix =
+        "var(--uchu-"
+            <> MisoString.toLower prefix
+            <> "-"
+            <> MisoString.toLower name
+            <> ")"
+
+extended :: MisoString -> Extended OKLCH Micro -> View Model Action
+extended name oklch =
+    li_
+        []
+        [ widget $ Heading Small name
+        , ul_
+            [class_ "colour"]
+            $ shade
+            <$> [1 .. 9]
+        ]
+  where
+    shade :: Int -> View Model Action
+    shade i =
+        let
+            bg, fg :: MisoString
+            bg = "background-color: " <> var i
+            fg =
+                if i > 5
+                    then "color: var(--uchu-yang)"
+                    else "color: var(--uchu-yin)"
+         in
+            li_
+                [ class_ "shade"
+                , styleInline_ $ MisoString.intercalate ";" [bg, fg]
+                ]
+                [ div_ [class_ "name"] [text $ toMisoString i]
+                , codes
+                    $ case i of
+                        1 -> oklch._1
+                        2 -> oklch._2
+                        3 -> oklch._3
+                        4 -> oklch._4
+                        5 -> oklch._5
+                        6 -> oklch._6
+                        7 -> oklch._7
+                        8 -> oklch._8
+                        _ -> oklch._9
+                ]
+    var i =
+        "var(--uchu-"
+            <> MisoString.toLower name
+            <> "-"
+            <> toMisoString i
+            <> ")"
+
+codes :: Color OKLCH Micro -> View Model Action
+codes oklch =
+    ul_
+        [class_ "codes"]
+        [ li_ [] [text $ toMisoString oklch]
+        , li_ [] [text rgbDec]
+        , li_ [] [text rgbHex]
+        ]
+  where
+    ColorSRGB r g b =
+        Colour.convertColor @(SRGB 'NonLinear)
+            $ let ColorOKLCH l c h = oklch
+               in ColorOKLCH (realToFrac l) (realToFrac c) (realToFrac h)
     rgbDec = "rgb(" <> dec r <> "," <> dec g <> "," <> dec b <> ")"
     rgbHex = "#" <> hex r <> hex g <> hex b
     word8 :: Double -> Word8
