f0aadec7bb8d425b6c46b6e0762ccb297d38beee
ners <ners@gmx.ch>
2026-04-03 17:34:02 +0200

    explicit switch colour tokens

--- dashi/lib/Dashi/Components/Switch.hs
+++ dashi/lib/Dashi/Components/Switch.hs
@@ -1,20 +1,52 @@
+{-# LANGUAGE OverloadedLists #-}
 {-# OPTIONS_GHC -Wno-missing-role-annotations #-}
 {-# OPTIONS_GHC -Wno-term-variable-capture #-}
 
 module Dashi.Components.Switch where
 
-import Clay hiding (checked, label, name, selected, span_, type_)
-import Clay qualified
+import Clay hiding (Background, checked, label, name, selected, span_, type_)
+import Clay qualified hiding (Background)
 import Dashi.Components.Util (ariaRole_)
 import Dashi.Prelude hiding ((#), (&))
 import Dashi.Style.Border (BorderColour (BorderColour, BorderFocusedColour))
-import Dashi.Style.Text (InverseTextColour (InverseTextColour))
+import Dashi.Style.Colour (LightDark)
+import Dashi.Style.Root (tokenDecl)
 import Dashi.Style.Tokens
+import Dashi.Style.Uchu (Uchu (Yang))
 import Dashi.Style.Util
 import Data.Coerce (coerce)
+import Data.Vector.Strict qualified as Vector
 import Miso.Html.Element (input_, label_, span_)
 import Miso.Html.Event (onChecked)
 import Miso.Html.Property (checked_, name_, type_)
+
+data Foreground = Foreground
+    deriving stock (Eq, Bounded, Enum)
+
+instance Token Foreground where
+    tokenName Foreground = "switch-foreground"
+
+instance ValueToken Foreground where
+    type ValueType Foreground = Uchu
+    tokenValue Foreground = Yang
+
+newtype Background = Background Bool
+    deriving stock (Eq, Bounded)
+
+allBackgrounds :: Vector Background
+allBackgrounds = Background <$> [minBound .. maxBound]
+
+instance Enum Background where
+    toEnum = (Vector.!) allBackgrounds
+    fromEnum = fromJust . flip Vector.findIndex allBackgrounds . (==)
+
+instance Token Background where
+    tokenName (Background checked) = "switch-background" <> bool "" "-checked" checked
+
+instance ValueToken Background where
+    type ValueType Background = LightDark Uchu
+    tokenValue (Background False) = tokenValue BorderColour
+    tokenValue (Background True) = tokenValue BorderFocusedColour
 
 data Switch model action = Switch
     { name :: MisoString
@@ -36,7 +68,10 @@ instance Widget (Switch model action) model action where
                 : checked_ checked
                 : onChecked (onChange . coerce @Checked)
                 : attrs
-    style =
+    style = do
+        ":root" ? do
+            tokenDecl @Foreground
+            tokenDecl @Background
         input # ariaRole "switch" ? do
             display flex
             flexDirection row
@@ -46,19 +81,19 @@ instance Widget (Switch model action) model action where
                 knobSize = heightEm
             width $ em widthEm
             height $ em heightEm
-            backgroundColor' BorderColour
+            backgroundColor' $ Background False
             border (em 0.1) solid transparent
             borderRadiusAll' XLarge
             transition "all" (sec 0.15) easeInOut 0
             before & do
                 important . content $ stringContent ""
-                backgroundColor' InverseTextColour
+                backgroundColor' Foreground
                 display block
                 aspectRatio 1
                 height $ pct 100
                 borderRadiusAll' XLarge
                 transition "margin" (sec 0.075) easeInOut 0
             Clay.checked & do
-                backgroundColor' BorderFocusedColour
+                backgroundColor' $ Background True
                 before & do
                     "margin-inline-start" ~:: em (widthEm - knobSize)
